075d90b4d2862a754563f0dc2323f84428de2d15
EmanElBoghdady <emaelboghdady807060504030@gmail.com>
2024-12-23 18:12:14 +0200

    ..

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react'
+import { AdminContext } from '../context/AdminContext'
+import { useNavigate } from 'react-router-dom';
+import {assets} from '../assets/assets';
+
+
+
+const Navbar = () => {
+
+    const { aToken, setAToken } = useContext(AdminContext); 
+
+    const navigate = useNavigate();
+
+
+    const logout =() =>{
+        navigate('/')
+        aToken && setAToken(' ');
+        aToken && localStorage.removeItem('aToken');
+    }
+
+  return (
+    <div className='flex justify-between items-center px-4 sm-px-10 border-b bg-white '>
+        <div className='flex items-center gap-2 text-xs'>
+            <img className='w-36 cm-w-40 cursor-pointer ' src={assets.admin_logo} alt='' />
+            <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>{aToken ? 'Admin' : 'Doctor'}</p>
+        </div>
+        <button onClick={logout}  className='cursor-pointer bg-primary text-white text-sm px-10 py-2 rounded-full'>Log out</button>
+      
+    </div>
+  )
+}
+
+export default Navbar
